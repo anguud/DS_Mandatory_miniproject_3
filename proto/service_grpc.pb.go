bb684a636011060770ff6ee5abfcf52841926194
Mr. Mikkelsen <bjmi@itu.dk>
2021-11-16 11:02:21 +0100

    generated protoc files

--- proto/service_grpc.pb.go
+++ proto/service_grpc.pb.go
@@ -14,86 +14,122 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// MutualExclusionClient is the client API for MutualExclusion service.
+// ProjectBidClient is the client API for ProjectBid service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type MutualExclusionClient interface {
-	RequestAccess(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+type ProjectBidClient interface {
+	Bid(ctx context.Context, in *Amount, opts ...grpc.CallOption) (*Ack, error)
+	Result(ctx context.Context, in *Message, opts ...grpc.CallOption) (*Outcome, error)
 }
 
-type mutualExclusionClient struct {
+type projectBidClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewMutualExclusionClient(cc grpc.ClientConnInterface) MutualExclusionClient {
-	return &mutualExclusionClient{cc}
+func NewProjectBidClient(cc grpc.ClientConnInterface) ProjectBidClient {
+	return &projectBidClient{cc}
 }
 
-func (c *mutualExclusionClient) RequestAccess(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
-	out := new(Response)
-	err := c.cc.Invoke(ctx, "/proto.mutual_exclusion/requestAccess", in, out, opts...)
+func (c *projectBidClient) Bid(ctx context.Context, in *Amount, opts ...grpc.CallOption) (*Ack, error) {
+	out := new(Ack)
+	err := c.cc.Invoke(ctx, "/proto.project_bid/bid", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// MutualExclusionServer is the server API for MutualExclusion service.
-// All implementations must embed UnimplementedMutualExclusionServer
+func (c *projectBidClient) Result(ctx context.Context, in *Message, opts ...grpc.CallOption) (*Outcome, error) {
+	out := new(Outcome)
+	err := c.cc.Invoke(ctx, "/proto.project_bid/result", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// ProjectBidServer is the server API for ProjectBid service.
+// All implementations must embed UnimplementedProjectBidServer
 // for forward compatibility
-type MutualExclusionServer interface {
-	RequestAccess(context.Context, *Request) (*Response, error)
-	mustEmbedUnimplementedMutualExclusionServer()
+type ProjectBidServer interface {
+	Bid(context.Context, *Amount) (*Ack, error)
+	Result(context.Context, *Message) (*Outcome, error)
+	mustEmbedUnimplementedProjectBidServer()
 }
 
-// UnimplementedMutualExclusionServer must be embedded to have forward compatible implementations.
-type UnimplementedMutualExclusionServer struct {
+// UnimplementedProjectBidServer must be embedded to have forward compatible implementations.
+type UnimplementedProjectBidServer struct {
 }
 
-func (UnimplementedMutualExclusionServer) RequestAccess(context.Context, *Request) (*Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method RequestAccess not implemented")
+func (UnimplementedProjectBidServer) Bid(context.Context, *Amount) (*Ack, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Bid not implemented")
 }
-func (UnimplementedMutualExclusionServer) mustEmbedUnimplementedMutualExclusionServer() {}
+func (UnimplementedProjectBidServer) Result(context.Context, *Message) (*Outcome, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Result not implemented")
+}
+func (UnimplementedProjectBidServer) mustEmbedUnimplementedProjectBidServer() {}
 
-// UnsafeMutualExclusionServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to MutualExclusionServer will
+// UnsafeProjectBidServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to ProjectBidServer will
 // result in compilation errors.
-type UnsafeMutualExclusionServer interface {
-	mustEmbedUnimplementedMutualExclusionServer()
+type UnsafeProjectBidServer interface {
+	mustEmbedUnimplementedProjectBidServer()
 }
 
-func RegisterMutualExclusionServer(s grpc.ServiceRegistrar, srv MutualExclusionServer) {
-	s.RegisterService(&MutualExclusion_ServiceDesc, srv)
+func RegisterProjectBidServer(s grpc.ServiceRegistrar, srv ProjectBidServer) {
+	s.RegisterService(&ProjectBid_ServiceDesc, srv)
 }
 
-func _MutualExclusion_RequestAccess_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Request)
+func _ProjectBid_Bid_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Amount)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(MutualExclusionServer).RequestAccess(ctx, in)
+		return srv.(ProjectBidServer).Bid(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/proto.mutual_exclusion/requestAccess",
+		FullMethod: "/proto.project_bid/bid",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MutualExclusionServer).RequestAccess(ctx, req.(*Request))
+		return srv.(ProjectBidServer).Bid(ctx, req.(*Amount))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// MutualExclusion_ServiceDesc is the grpc.ServiceDesc for MutualExclusion service.
+func _ProjectBid_Result_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Message)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProjectBidServer).Result(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.project_bid/result",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProjectBidServer).Result(ctx, req.(*Message))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// ProjectBid_ServiceDesc is the grpc.ServiceDesc for ProjectBid service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var MutualExclusion_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "proto.mutual_exclusion",
-	HandlerType: (*MutualExclusionServer)(nil),
+var ProjectBid_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "proto.project_bid",
+	HandlerType: (*ProjectBidServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "requestAccess",
-			Handler:    _MutualExclusion_RequestAccess_Handler,
+			MethodName: "bid",
+			Handler:    _ProjectBid_Bid_Handler,
+		},
+		{
+			MethodName: "result",
+			Handler:    _ProjectBid_Result_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
